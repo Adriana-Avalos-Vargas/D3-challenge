d92d65be5e7f7865abaa79395524a2ca6c7abaaf
ADRIANA AVALOS VARGAS <avalos_84@hotmail.com>
2020-11-16 22:03:19 -0600

    final hw with axis names

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -92,7 +92,19 @@ d3.csv("data.csv").then(function(usData) {
     .attr("fill", "red")
     .text(function(d) { return d.abbr; });
 
-    
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Lacks healthcare (%)");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("In Poverty (%)");
 
 }).catch(function(error) {
   console.log("No lo lee")
